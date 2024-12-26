3ebddbef739b7c4520846e975002940ef801df38
Sarang Nagpal <sarangng@amazon.com>
2024-12-26 22:26:49 +0530

    support different delimiters, make all test cases pass

--- calculator.js
+++ calculator.js
@@ -1,9 +1,13 @@
 export const add = (numbers) => {
-    const numbersArray = numbers.split(/[\n,]/);
-
-    if(isNaN(parseInt(numbersArray[1]))) {
-        return numbers;
+    let numbersArray = [];
+    if(numbers.startsWith('//')) {
+        const [delimiterString, numbersString] = numbers.split('\n');
+        const delimiter = delimiterString.slice(2);
+        numbersArray = numbersString.split(delimiter);
     }
+    else numbersArray = numbers.split(/[\n,]/);
+
+    if(numbersArray.length === 1 && isNaN(parseInt(numbersArray[0]))) return 0;
 
     return numbersArray.reduce((sum, number) => (sum + parseInt(number)), 0);
 }
