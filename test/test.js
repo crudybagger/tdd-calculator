bb8265af3957fa2becd4415419f6a782d332cc04
Sarang Nagpal <sarangng@amazon.com>
2024-12-26 19:52:32 +0530

    add failing test cases for different delimiter and multiple numbers

--- test/test.js
+++ test/test.js
@@ -17,4 +17,11 @@ describe('test add', function () {
   it('new line between numbers', function () {
     equal(add("1\n2, 3"), 6);
   });
+  it('different delimiters', function () {
+    equal(add("//;\n1;2"), 3);
+  });
+  it('different delimiters multiple numbers', function () {
+    equal(add("//*\n1*2*12"), 15);
+  });
+
 });
