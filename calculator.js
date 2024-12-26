5008682d097e358382980474eaec6ec8e6dcc21a
Sarang Nagpal <sarangng@amazon.com>
2024-12-26 19:37:07 +0530

    handle 2 numbers case and solve test cases

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
 export const add = (numbers) => {
-    return numbers;
+    const [num1, num2] = numbers.split(",");
+
+    if(isNaN(parseInt(num2))) {
+        return numbers;
+    }
+
+    return parseInt(num1) + parseInt(num2);
 }
